a303fad1dd12b01ae1b11fbc1a10a12d6ac2b1b7
Ben Bradley <ben@bradleyit.com>
2015-10-10 16:23:07 -0700

    working router
    I updated the deps and took the querystring example from the
    react-router repo

--- src/public/index.js
+++ src/public/index.js
@@ -1,54 +1,47 @@
 'use strict';
+import React from 'react'
+// import { render } from 'react-dom'
+import { createHistory, useBasename } from 'history'
+import { Router, Route, Link } from 'react-router'
 
-import React from 'react';
-import Router from 'react-router';
-// import { Router, Route, Link } from 'react-router';
+const history = useBasename(createHistory)({
+  basename: '/query-params'
+})
 
-let { Route, Link } = Router;
+class User extends React.Component {
+  render() {
+    let { userID } = this.props.params
+    let { query } = this.props.location
+    let age = query && query.showAge ? '33' : ''
+
+    return (
+      <div className="User">
+        <h1>User id: {userID}</h1>
+        {age}
+      </div>
+    )
+  }
+}
 
 class App extends React.Component {
   render() {
-    return <div>
-      <h1>This is the APP!</h1>
-      <ul>
-        <li>links follow</li>
-        <li><Link to="/foo">foo</Link></li>
-        <li><Link to="/bar">bar</Link></li>
-      </ul>
-      {this.props.children}
-    </div>;
+    return (
+      <div>
+        <ul>
+          <li><Link to="/user/bob" activeClassName="active">Bob</Link></li>
+          <li><Link to="/user/bob" query={{ showAge: true }} activeClassName="active">Bob With Query Params</Link></li>
+          <li><Link to="/user/sally" activeClassName="active">Sally</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
   }
 }
 
-class Main extends React.Component {
-  render() {
-    console.log('rendering Main');
-    return <h1>MAIN!</h1>;
-  }
-}
-
-class Foo extends React.Component {
-  render() {
-    console.log('rendering Foo');
-    return <h1>FOO!</h1>;
-  }
-}
-
-class Bar extends React.Component{
-  render() {
-    console.log('rendering Bar');
-    return <h1>BAR!</h1>;
-  }
-}
-
-const routes = <Route handler={App}>
-  <Route path="/" component={Main} />
-  <Route path="foo" component={Foo} />
-  <Route path="bar" component={Bar} />
-</Route>;
-
-Router.run(routes, function(Handler) {
-  React.render(<Handler />, document.getElementById('app'));
-});
-
-// React.render(<Router routes={routes} />, document.body);
+React.render((
+  <Router history={history}>
+    <Route path="/" component={App}>
+      <Route path="user/:userID" component={User} />
+    </Route>
+  </Router>
+), document.getElementById('app'))
